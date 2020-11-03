70ce637bbbe636cfebd1834112464f82eeffcf88
junholee <juno7803@naver.com>
2020-11-04 02:57:28 +0900

    전체 Component 구조 잡기

---
+++ 3rd_seminar/github-finder/src/components/searchInput/SearchInput.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SearchInput() {
+  return (
+    <>
+      <form>
+        <input type="text" placeholder="Github 프로필을 검색해보세요" />
+      </form>
+    </>
+  );
+}
+
+export default SearchInput;
---
+++ 3rd_seminar/github-finder/src/components/searchResult/SearchResult.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SearchResult() {
+  return (
+    <>
+      <div>
+       SearchResult
+      </div>
+    </>
+  );
+}
+
+export default SearchResult;
